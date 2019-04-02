3e40c80d3a19a2804b7f0740f76064d2682ea6d8
djalma <djalminhajr_cunha@hotmail.com>
2019-04-02 18:00:40 -0300

    agora da para salvar o endereço da pessoa, porem esta bugado ainda

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Pessoa oi = new Pessoa();
+	oi.url("https://www.google.com.br/maps/uv?hl=pt-BR&pb=!1s0x94ce4158ef9c7c05:0x776b798985695f52!2m22!2m2!1i80!2i80!3m1!2i20!16m16!1b1!2m2!1m1!1e1!2m2!1m1!1e3!2m2!1m1!1e5!2m2!1m1!1e4!2m2!1m1!1e6!3m1!7e115!4s/maps/place/fei/@-23.7240687,-46.5793847,3a,75y,184h,90t/data%3D*213m4*211e1*213m2*211s81onwgy4UPPwnf-940FV_w*212e0*214m2*213m1*211s0x94ce4158ef9c7c05:0x776b798985695f52?sa%3DX!5sfei+-+Pesquisa+Google&imagekey=!1e2!2s81onwgy4UPPwnf-940FV_w&sa=X&ved=2ahUKEwi-sY-KprLhAhWIDrkGHZMgDroQpx8wEXoECAoQCw");
     }
 }
--- src/com/company/Pessoa.java
+++ src/com/company/Pessoa.java
@@ -1,11 +1,39 @@
 package com.company;
 
-public class Pessoa {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+public class Pessoa {
+/** a função abaixo serve para salvar a url de posição da pessoa */
+    public void url(String html) {
+        try {
+            URL local = new URL(html);
+            String texto;
+            InputStreamReader stream = new InputStreamReader(local.openStream());
+            PrintWriter saida = new PrintWriter("arqXml.html");
+            BufferedReader input = new BufferedReader(stream);
+            while ((texto = input.readLine()) != null) {
+                saida.println(texto);
+            }
+            saida.flush();
+            saida.close();
+        } catch (
+                MalformedURLException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (
+                IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
     public String nome;
     public String mensagem;
-    // a variavel abaixo esta com o tipo errado mas sera para a posição da pessoa
-    public int local;
+    public URL local;
 
 }

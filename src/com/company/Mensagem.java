fef2af238a6018b70e665836570305239fcc0b7b
djalma <djalminhajr_cunha@hotmail.com>
2019-04-02 16:32:30 -0300

    agora tem detalhamento

--- src/com/company/Mensagem.java
+++ src/com/company/Mensagem.java
@@ -1,13 +1,17 @@
 package com.company;
-
+/**
+inicialmente farei os vetores estaticos simplismente para aplicar a logica
+ */
 public class Mensagem {
 
+    /** a funcao abaixo serve para o usuario inserir uma mensagem*/
     public void inserirTxt(String ind, String txt){
         eu[posissao].mensagem = txt;
         eu[posissao].nome = ind;
         posissao++;
     }
 
+    /** a funcao abaixo retorna a mensagem */
     public String imprimeTxt(String ind){
         for (int i = 0; i < 10; i++){
             if(eu[i].nome == ind){
@@ -17,10 +21,12 @@ public class Mensagem {
         return "mensagem não encontrada";
     }
 
+    /** a funcao serve para darmos um tamanho estatico ao vetor */
     private void setTam(int tam) {
         this.tam = tam;
     }
 
+    /** a funcao retorna o tamanho do vetor */
     private int getTam() {
         return tam;
     }
--- src/com/company/Pessoa.java
+++ src/com/company/Pessoa.java
@@ -2,7 +2,10 @@ package com.company;
 
 public class Pessoa {
 
+
     public String nome;
     public String mensagem;
+    // a variavel abaixo esta com o tipo errado mas sera para a posição da pessoa
+    public int local;
 
 }

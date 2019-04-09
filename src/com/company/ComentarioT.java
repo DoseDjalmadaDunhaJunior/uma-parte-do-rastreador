4822d99db90ef38dc5810199f733d5727dc23299
djalma <djalminhajr_cunha@hotmail.com>
2019-04-09 20:25:50 -0300

    pega uma informação e manda para outra função

--- src/com/company/ComentarioT.java
+++ src/com/company/ComentarioT.java
@@ -1,4 +1,10 @@
 package com.company;
 
 public class ComentarioT {
+
+    public void mostrar(String txt,String oi){
+        System.out.println(txt);
+        System.out.println(oi);
+
+    }
 }
--- src/com/company/PublicacoesT.java
+++ src/com/company/PublicacoesT.java
@@ -1,4 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class PublicacoesT {
+
+    public void setNome(){
+        Scanner bot = new Scanner(System.in);
+        nome = bot.nextLine();
+    }
+
+    public void setComentario(){
+        Scanner bot = new Scanner(System.in);
+        comentario = bot.nextLine();
+    }
+
+    public void getTudo(){
+    if(nome != null && comentario != null){
+        ComentarioT tesao = new ComentarioT();
+        tesao.mostrar(nome,comentario);
+    }
+    else{
+        System.out.println("algum campo esta vazio");
+    }
+    }
+
+    private String nome = null;
+    private String comentario = null;
 }

77b86c7d8094c29aa372c713eb7eaa5542a21242
djalma <djalminhajr_cunha@hotmail.com>
2019-04-09 20:06:37 -0300

    para andar um pouco sem a parte grafica

--- src/com/company/AdmComentario.java
+++ src/com/company/AdmComentario.java
@@ -1,11 +1,21 @@
 package com.company;
-import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-public class AdmComentario {
+import static javafx.application.Application.launch;
+
+public class AdmComentario extends Application {
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("visual/comentario.fxml"));
+        primaryStage.setScene(new Scene(root,300,200));
+        primaryStage.show();
+    }
+
+    AdmComentario(){
+        String[] args;
+        //launch(args);
+    }
+
 }
